85804551077a4b003fbc03d2f0a457a97456e2a2
sandhiya-gh <sandhiyakennedy3008@gmail.com>
2025-04-29 20:48:46 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const pool = require('./dbConfig'); // Updated path to include config folder
+const pool = require('./dbConfig'); 
 const app = express();
 
 //middleware 
